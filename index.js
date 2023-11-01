02b1e84307335fc611ed2d4ef408a3da4f97d393
shamamabinshakil <dev.shamama@gmail.com>
2023-11-01 13:18:49 +0500

    inital state 1

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const prisma = new PrismaClient();
 
+// app.get("/", async (req, res) => {
+//     const user = await prisma.user.findMany()
+//     res.json(user)
+// });
 
 app.get("/", async (req, res) => {
-    const user = await prisma.user.findMany()
-    res.json(user)
-});
-app.get("/api", async (req, res) => {
-    
-    res.json({msg: "ok"})
+  res.json({ msg: "ok" });
 });
 
 app.listen(PORT, () => {
